56e88fd5e486058d428355b726beeaef27e34959
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-19 10:17:41 +0200

    added components structure

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,3 +1,12 @@
+import Footer from "@/components/shared/footer/Footer";
+import Header from "@/components/shared/footer/header/Header";
+
 export default function CheckoutPage() {
-  return <div>page</div>;
+  return (
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  );
 }
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import Footer from "@/components/shared/footer/Footer";
+import Header from "@/components/shared/footer/header/Header";
 
 export default function MenuPage() {
-  return <div>page</div>;
+  return (
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  );
 }
