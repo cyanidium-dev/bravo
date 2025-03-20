aa7eb2c5cc479ed92e2232b352dccee3840be1a8
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-20 10:21:03 +0200

    added offers list

--- src/components/home/offer/Offer.tsx
+++ src/components/home/offer/Offer.tsx
@@ -1,3 +1,16 @@
+import SectionTitle from "@/components/shared/titles/SectionTitle";
+
 export default function Offer() {
-  return <section className="bg-green pt-16 pb-[70px]">Offer</section>;
+  return (
+    <section className="bg-green pt-16 pb-[70px]">
+      <div className="lg:flex lg:justify-between lg:items-center mb-[225px] lg:mb-[147px]">
+        <SectionTitle variant="white" className="mb-5 lg:mb-0">
+          Наша унікальна пропозиція
+        </SectionTitle>
+        <p className="text-white">
+          Ми – це не просто доставка їжі, а ваш партнер у здоровому харчуванні!
+        </p>
+      </div>
+    </section>
+  );
 }
---
+++ src/components/home/offer/OffersList.tsx
@@ -0,0 +1,23 @@
+export default function OffersList() {
+  const offersList = [
+    {
+      subtitle: "Швидка доставка",
+      description: "Щоб їжа була завжди свіжою",
+      quantity: "30 хв",
+      text: "Середній час доставки",
+    },
+    {
+      subtitle: "Нешкідливе приготування",
+      description: "Готуємо на мангалі без зайвої олії",
+      quantity: "98%",
+      text: "Задоволених клієнтів",
+    },
+    {
+      subtitle: "Безкоштовна доставка",
+      description: "Якщо ви оплачуєте готівкою",
+      quantity: "90%",
+      text: "Клієнтів використовують це",
+    },
+  ];
+  return <ul>s</ul>;
+}
