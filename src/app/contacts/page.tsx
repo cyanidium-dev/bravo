07e6b9564d9b176d8a91152e3577d39ab6856f02
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-21 23:13:39 +0200

    added map

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,6 +1,5 @@
 import ContactsInfo from "@/components/contacts/contactsInfo/ContactsInfo";
 import Hero from "@/components/contacts/hero/Hero";
-import Map from "@/components/contacts/map/Map";
 import Footer from "@/components/shared/footer/Footer";
 import Header from "@/components/shared/header/Header";
 
@@ -11,7 +10,6 @@ export default function ContactsPage() {
       <main className="flex-1">
         <Hero />
         <ContactsInfo />
-        <Map />
       </main>
       <Footer />
     </>
--- src/components/contacts/contactsInfo/ContactsInfo.tsx
+++ src/components/contacts/contactsInfo/ContactsInfo.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import ContactsList from "./ContactsList";
+
 export default function ContactsInfo() {
-  return <div>ContactsInfo</div>;
+  return (
+    <section className="container xl:max-w-[1280px] py-[120px] xl:pt-20">
+      <ContactsList />
+      <div className="mt-[100px] sm:mt-[44px] w-full h-[245px] rounded-[16px] overflow-hidden">
+        <Image
+          src="/images/contacts/contactsInfo/map.webp"
+          alt="map"
+          width={2280}
+          height={490}
+          className="w-full h-full object-cover object-center"
+        />
+      </div>
+    </section>
+  );
 }
