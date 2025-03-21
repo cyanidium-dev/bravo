a0b19cd49d80d29fff889c1ff1f90c3c1a61a45f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-21 17:18:48 +0200

    made refactoring

--- src/components/shared/pagination/Pagination.tsx
+++ src/components/shared/pagination/Pagination.tsx
@@ -1,16 +1,37 @@
+"use client";
+import { useState, useLayoutEffect, ReactNode } from "react";
 import ArrowInCircleIcon from "../icons/ArrowinCircleIcon";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useItemsPerPage } from "@/hooks/useItemsPerPage";
 
-export default function Pagination({
-  currentPage,
-  totalPages,
-  maxVisiblePages = 4,
-  onPageChange,
-}: {
-  currentPage: number;
-  totalPages: number;
+interface PaginationProps<T> {
+  items: T[];
+  renderItems: (items: T[]) => ReactNode;
+  scrollTargetId: string;
   maxVisiblePages?: number;
-  onPageChange: (page: number) => void;
-}) {
+}
+
+export default function Pagination<T>({
+  items,
+  renderItems,
+  scrollTargetId,
+  maxVisiblePages = 4,
+}: PaginationProps<T>) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const page = Number(searchParams.get("page")) || 1;
+  const [currentPage, setCurrentPage] = useState(page);
+  const itemsPerPage = useItemsPerPage();
+  const totalPages = Math.ceil(items.length / itemsPerPage);
+  const currentItems = items.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  useLayoutEffect(() => {
+    setCurrentPage(parseInt(searchParams.get("page") || "1", 10));
+  }, [searchParams]);
+
   const generatePageNumbers = () => {
     const pageNumbers = [];
     const halfVisible = Math.floor(maxVisiblePages / 2);
@@ -31,45 +52,71 @@ export default function Pagination({
 
   const pageNumbers = generatePageNumbers();
 
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("page", page.toString());
+    router.push(`?${params.toString()}`, { scroll: false });
+
+    const targetElement = document.getElementById(scrollTargetId);
+    if (targetElement) {
+      const yOffset = -120; // Зміщення на 120px вгору
+      const yPosition =
+        targetElement.getBoundingClientRect().top +
+        window.pageYOffset +
+        yOffset;
+
+      window.scrollTo({
+        top: yPosition,
+      });
+    }
+  };
+
   return (
-    <div className="inline-flex justify-center items-center gap-[35px]">
-      <button
-        className={`flex justify-center items-center p-[16.5px] size-[66px] rounded-[16px] 
+    <>
+      <div className="w-full max-w-[320px] sm:max-w-[660px] lg:max-w-[1000px] xl:max-w-full mb-10 xl:mb-[62px]">
+        {renderItems(currentItems)}
+      </div>
+      <div className="inline-flex justify-center items-center gap-[35px]">
+        <button
+          className={`flex justify-center items-center p-[16.5px] size-[66px] rounded-[16px] 
           border-2 border-black transition duration-300 ease-in-out
           enabled:hover:bg-black/85 enabled:active:scale-95 enabled:focus-visible:bg-black/85
-          ${currentPage === 1 ? "bg-white text-black" : "bg-black text-white"}`}
-        onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        <ArrowInCircleIcon className="rotate-180" />
-      </button>
+          ${page === 1 ? "bg-white text-black" : "bg-black text-white"}`}
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={page === 1}
+        >
+          <ArrowInCircleIcon className="rotate-180" />
+        </button>
 
-      <div>
-        {pageNumbers.map((page) => (
-          <button
-            key={page}
-            className={`px-[9px] py-2 text-16med leading-[123%] transition duration-300 ease-in-out
+        <div>
+          {pageNumbers.map((page) => (
+            <button
+              key={page}
+              className={`px-[9px] py-2 text-16med leading-[123%] transition duration-300 ease-in-out
             ${page === currentPage ? " text-green" : "  hover:text-green"}`}
-            onClick={() => onPageChange(page)}
-          >
-            {page}
-          </button>
-        ))}
-      </div>
+              onClick={() => handlePageChange(page)}
+            >
+              {page}
+            </button>
+          ))}
+        </div>
 
-      <button
-        className={`flex justify-center items-center p-[16.5px] size-[66px] rounded-[16px] transition duration-300 ease-in-out
+        <button
+          className={`flex justify-center items-center p-[16.5px] size-[66px] rounded-[16px] transition duration-300 ease-in-out
            border-2 border-black enabled:hover:bg-black/85 enabled:active:scale-95 enabled:focus-visible:bg-black/85
           ${
             currentPage === totalPages
               ? "text-black bg-white"
               : "bg-black text-white"
           }`}
-        onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-      >
-        <ArrowInCircleIcon />
-      </button>
-    </div>
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={page === totalPages}
+        >
+          <ArrowInCircleIcon />
+        </button>
+      </div>
+    </>
   );
 }
