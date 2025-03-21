54f5d0c6bbab51e998a3ce572a19bec85302d482
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-21 18:44:23 +0200

    added find article by slug query

--- src/app/blog/[article]/page.tsx
+++ src/app/blog/[article]/page.tsx
@@ -1,11 +1,33 @@
+import Content from "@/components/article/content/Content";
+import Hero from "@/components/article/hero/Hero";
 import Footer from "@/components/shared/footer/Footer";
 import Header from "@/components/shared/header/Header";
+import { GET_ARTICLE_BY_SLUG } from "@/lib/datoCmsQueries";
+import { getDataFromCmsByField } from "@/utils/getDataFromCmsByField";
 
-export default function ArticlePage() {
+interface ArticlePageProps {
+  params: Promise<{ article: string }>;
+}
+
+export default async function ArticlePage({ params }: ArticlePageProps) {
+  const { article } = await params;
+
+  const res = await getDataFromCmsByField(GET_ARTICLE_BY_SLUG, article);
+  const currentArticle = res?.data?.allArticles;
+
+  console.log(currentArticle);
+
+  if (!currentArticle) {
+    return null;
+  }
+
   return (
     <>
       <Header />
-      <main className="flex-1"></main>
+      <main className="flex-1">
+        <Hero />
+        <Content />
+      </main>
       <Footer />
     </>
   );
---
+++ src/utils/getDataFromCms.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export async function getDataFromCms(query: string) {
+  try {
+    const response = await axios({
+      method: "post",
+      url: `${BASE_URL}api/datocms`,
+      data: {
+        query,
+        variables: {},
+        includeDrafts: false,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+}
