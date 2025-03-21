6ad91c9fbba64301274adf43f04bff95c009e1de
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-21 23:26:12 +0200

    updated footer

---
+++ src/components/contacts/contactsInfo/ContactItem.tsx
@@ -0,0 +1,3 @@
+export default function ContactItem() {
+  return <li>ContactItem</li>;
+}
--- src/components/contacts/contactsInfo/ContactsList.tsx
+++ src/components/contacts/contactsInfo/ContactsList.tsx
@@ -1,5 +1,18 @@
+import { PHONE, EMAIL, ADDRESS_FIRST } from "@/constants/constants";
+import ContactItem from "./ContactItem";
+
 export default function ContactsList() {
+  const contactsList = [
+    { icon: "", title: "Номер телефону", description: PHONE },
+    { icon: "", title: "Номер телефону", description: EMAIL },
+    { icon: "", title: "Номер телефону", description: ADDRESS_FIRST },
+  ];
+
   return (
-    <div>ContactsList</div>
-  )
+    <ul>
+      {contactsList.map((contact, idx) => (
+        <ContactItem key={idx} />
+      ))}
+    </ul>
+  );
 }
