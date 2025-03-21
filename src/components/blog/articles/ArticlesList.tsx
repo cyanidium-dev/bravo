0281c6267bc3dddc98bcc1a04149c7b30b0a5c97
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-21 18:01:48 +0200

    added suspense to the pagination

--- src/components/blog/articles/ArticlesList.tsx
+++ src/components/blog/articles/ArticlesList.tsx
@@ -3,6 +3,7 @@
 import { Article } from "@/types/article";
 import ArticleItem from "./ArticleItem";
 import Pagination from "@/components/shared/pagination/Pagination";
+import { Suspense } from "react";
 
 interface ArticlesListProps {
   articles: Article[];
@@ -13,20 +14,22 @@ const SECTION_ID = "articles-list";
 export default function ArticlesList({ articles }: ArticlesListProps) {
   return (
     <div id={SECTION_ID} className="flex flex-col justify-center items-center">
-      <Pagination
-        items={articles}
-        scrollTargetId={SECTION_ID}
-        renderItems={(currentItems) => (
-          <ul
-            className=" flex flex-col gap-y-5 
+      <Suspense>
+        <Pagination
+          items={articles}
+          scrollTargetId={SECTION_ID}
+          renderItems={(currentItems) => (
+            <ul
+              className=" flex flex-col gap-y-5 
           sm:flex-row sm:flex-wrap sm:gap-y-5 sm:gap-x-4 xl:gap-x-5 xl:gap-y-6"
-          >
-            {currentItems.map((article) => (
-              <ArticleItem key={article.id} article={article} />
-            ))}
-          </ul>
-        )}
-      />
+            >
+              {currentItems.map((article) => (
+                <ArticleItem key={article.id} article={article} />
+              ))}
+            </ul>
+          )}
+        />
+      </Suspense>
     </div>
   );
 }
