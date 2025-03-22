4b07b1f768572d9d4cf0166e500a6d969741d91d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-22 20:17:03 +0200

    updated callback button

--- src/components/shared/header/CallbackInfo.tsx
+++ src/components/shared/header/CallbackInfo.tsx
@@ -1,14 +1,26 @@
 import GreenButton from "../buttons/GreenButton";
+import { PHONE } from "@/constants/constants";
+import { headerPhoneRegex } from "@/regex/regex";
+import PhoneIcon from "../icons/PhoneIcon";
 
 export default function CallbackInfo() {
   return (
-    <div>
-      <h2 className="mt-7 mb-[26px] text-36bold uppercase leading-[130%]">
+    <div className="max-w-[380px] sm:mx-auto">
+      <h2 className="mt-7 mb-[26px] text-36bold uppercase leading-[130%] sm:text-center">
         Маєте додаткові питання?
       </h2>
-      <GreenButton className="w-full max-w-[360px] mb-12 text-white">
-        Замовити дзвінок
-      </GreenButton>
+      <a
+        href={`tel:+${PHONE.replace(/\D/g, "")}`}
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+        aria-label="phone number"
+        className="block sm:mx-auto"
+      >
+        <GreenButton className="flex gap-x-[14px] items-center w-full max-w-[360px] mb-12 sm:mx-auto text-white">
+          <PhoneIcon className="size-6" />
+          {PHONE.replace(headerPhoneRegex, "$1-$2-$3-$4-$5")}
+        </GreenButton>
+      </a>
     </div>
   );
 }
