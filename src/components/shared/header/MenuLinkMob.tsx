5f7eb30c77db16f3ce3bad126c2b16756ad2c402
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-22 20:07:33 +0200

    updated burger menu

--- src/components/shared/header/MenuLinkMob.tsx
+++ src/components/shared/header/MenuLinkMob.tsx
@@ -19,12 +19,12 @@ export default function MenuLinkMob({
 
   return (
     <li
-      className={`relative pt-6 pb-4 before:content-[''] before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:bg-black ${className}`}
+      className={`relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:bg-black ${className}`}
     >
       <Link
         href={link}
         onClick={() => setIsHeaderMenuOpened(false)}
-        className="leading-[120%] transition duration-300 ease-out active:text-green focus-visible:text-green
+        className="block w-full  pt-6 pb-4 leading-[120%] transition duration-300 ease-out active:text-green focus-visible:text-green
          laptop:hover:text-green outline-none"
       >
         {title}
