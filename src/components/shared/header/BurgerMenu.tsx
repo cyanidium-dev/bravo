0203b42442d622a2e80455d896cb9187bf1b967e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-24 23:18:25 +0200

    updated burger menu

--- src/components/shared/header/BurgerMenu.tsx
+++ src/components/shared/header/BurgerMenu.tsx
@@ -19,18 +19,18 @@ export default function BurgerMenu({
         isHeaderMenuOpened
           ? "translate-x-0 no-doc-scroll opacity-100"
           : "-translate-x-full opacity-0"
-      } lg:hidden absolute z-50 top-0 right-0 w-[100vw] h-[100dvh] pb-10 bg-white text-black
+      } lg:hidden absolute z-50 top-0 right-0 w-[100vw] h-[100dvh] pb-8 bg-white text-black
       transition duration-[600ms] overflow-y-auto`}
     >
       <div className="container">
-        <div className="container sticky z-[60] top-0 left-[30px] flex items-center justify-between py-4 pr-[54px] sm:pr-[100px] mx-auto bg-white">
+        <div className="sticky z-[60] top-0 left-[30px] flex items-center justify-between py-4 pr-[54px] sm:pr-[100px] mx-auto bg-white">
           <Logo
             setIsHeaderMenuOpened={setIsHeaderMenuOpened}
             className="text-24bold leading-[120%] relative z-[60] text-black"
           />
           <CartButton variant="black" />
         </div>
-        <CallbackInfo />
+        <CallbackInfo setIsHeaderMenuOpened={setIsHeaderMenuOpened} />
         <NavMenuMob setIsHeaderMenuOpened={setIsHeaderMenuOpened} />
       </div>
     </div>
