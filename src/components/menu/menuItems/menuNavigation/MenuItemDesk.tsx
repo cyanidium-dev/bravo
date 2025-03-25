79971af98ec0a552635ada4cea04b6190d2f2100
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-25 22:44:35 +0200

    updated menu navigation item

--- src/components/menu/menuItems/menuNavigation/MenuItemDesk.tsx
+++ src/components/menu/menuItems/menuNavigation/MenuItemDesk.tsx
@@ -12,7 +12,7 @@ export default function MenuItemDesk({
 }: MenuItemDeskProps) {
   return (
     <li
-      className={`group px-6 py-2 min-w-[270px] rounded-full border active:scale-[95%] xl:hover:border-green/80 xl:hover:bg-green/80
+      className={`group min-w-[270px] rounded-full border active:scale-[95%] xl:hover:border-green/80 xl:hover:bg-green/80
          focus-visible:bg-green/80 focus-visible:border-green/80 transition duration-300 ease-in-out ${
            category.url === currentCategory
              ? " border-green bg-green"
@@ -21,7 +21,7 @@ export default function MenuItemDesk({
     >
       <Link
         href={`/menu/${category?.url}`}
-        className={`flex items-center justify-between ${
+        className={`flex items-center justify-between px-6 py-2 ${
           category.url === currentCategory ? "text-white" : "text-black"
         } xl:group-hover:text-white xl:group-[focus-visible]:text-white transition duration-300 ease-in-out`}
       >
