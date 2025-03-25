c514751dda3b8b1bf38011b7d34143d565fcc26b
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-25 22:40:32 +0200

    update cart total styles

--- src/components/shared/modals/cartModal/CartTotal.tsx
+++ src/components/shared/modals/cartModal/CartTotal.tsx
@@ -18,7 +18,7 @@ export default function CartTotal({ setIsPopUpShown }: CartTotalProps) {
 
   return (
     <div className="fixed bottom-0 left-0 w-full px-[30px] lg:px-9 py-9 bg-black rounded-tr-[16px] rounded-tl-[16px]">
-      <div className="flex flex-row justify-between mb-8 lg:mb-7 text-white">
+      <div className="flex flex-row items-center justify-between mb-8 lg:mb-7 text-white">
         <p>Загальна вартість</p>
         <p className="text-20med lg:text-24med leading-[123%]">
           {total}&nbsp;грн
