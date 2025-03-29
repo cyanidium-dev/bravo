d6ec85315fb45c513d214ecfcc88e34859847b2d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-29 11:35:55 +0200

    added mission section animation

--- src/components/about/mission/Mission.tsx
+++ src/components/about/mission/Mission.tsx
@@ -1,20 +1,35 @@
 import SectionTitle from "@/components/shared/titles/SectionTitle";
 import Image from "next/image";
+import AnimatedWrapper from "@/components/shared/animatedWrappers/AnimatedWrapper";
+import { fadeInAnimation } from "@/helpers/animation";
+import { motion } from "framer-motion";
 
 export default function Mission() {
   return (
     <section className="overflow-hidden">
       <div className="container xl:max-w-[1280px] py-[120px]">
         <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-y-[18px] mb-[60px] tab:mb-[58px]">
-          <SectionTitle className="md:w-[53.6%]">Наша місія</SectionTitle>
-          <p className="md:w-[39%]">
+          <AnimatedWrapper
+            animation={fadeInAnimation({ y: 30 })}
+            className="md:w-[53.6%]"
+          >
+            <SectionTitle>Наша місія</SectionTitle>
+          </AnimatedWrapper>
+          <AnimatedWrapper
+            as={motion.p}
+            animation={fadeInAnimation({ y: 30 })}
+            className="md:w-[39%]"
+          >
             Ми хочемо зробити здоровий спосіб життя простим і приємним. Готуючи
             для вас, ми обираємо тільки натуральні, свіжі та екологічні
             продукти, щоб кожен прийом їжі приносив максимум користі.
-          </p>
+          </AnimatedWrapper>
         </div>
         <div className="flex flex-col gap-y-5 md:flex-row justify-between mb-[341px] md:mb-[123px]">
-          <div className="w-full md:w-[53.6%] aspect-[300/200] md:aspect-[611/407] rounded-[12px] md:rounded-[18px] overflow-hidden">
+          <AnimatedWrapper
+            animation={fadeInAnimation({ x: -30 })}
+            className="w-full md:w-[53.6%] aspect-[300/200] md:aspect-[611/407] rounded-[12px] md:rounded-[18px] overflow-hidden"
+          >
             <Image
               src="/images/about/mission/imageFirst.webp"
               alt="woman with phone"
@@ -22,9 +37,12 @@ export default function Mission() {
               height={910}
               className="w-full h-full object-cover"
             />
-          </div>
+          </AnimatedWrapper>
           <div className="relative md:w-[44.6%]">
-            <div className="w-full aspect-[300/200] md:aspect-[509/328] mb-5 rounded-[12px] md:rounded-[18px] overflow-hidden">
+            <AnimatedWrapper
+              animation={fadeInAnimation({ x: 30 })}
+              className="w-full aspect-[300/200] md:aspect-[509/328] mb-5 rounded-[12px] md:rounded-[18px] overflow-hidden"
+            >
               <Image
                 src="/images/about/mission/imageSecond.webp"
                 alt="two people"
@@ -32,19 +50,29 @@ export default function Mission() {
                 height={910}
                 className="w-full h-full object-cover"
               />
-            </div>
-            <SectionTitle>Радість смаку</SectionTitle>
-            <Image
-              src="/images/about/mission/tomatoes.webp"
-              alt="tomatoe"
-              width={696}
-              height={614}
-              className="md:hidden absolute -z-10 bottom-[-312px] left-[-87px] w-[348px] h-auto"
-            />
+            </AnimatedWrapper>
+            <AnimatedWrapper animation={fadeInAnimation({ y: 30, delay: 0 })}>
+              <SectionTitle>Радість смаку</SectionTitle>
+            </AnimatedWrapper>
+            <AnimatedWrapper
+              animation={fadeInAnimation({ scale: 0.8, delay: 0.8 })}
+              className="md:hidden absolute -z-10 bottom-[-312px] left-[-87px]"
+            >
+              <Image
+                src="/images/about/mission/tomatoes.webp"
+                alt="tomatoe"
+                width={696}
+                height={614}
+                className="w-[348px] h-auto"
+              />
+            </AnimatedWrapper>
           </div>
         </div>
         <div className="relative flex flex-col md:flex-row md:justify-between gap-y-[60px] md:gap-x-5">
-          <div className="flex flex-col gap-y-[18px] md:flex-col-reverse md:justify-end md:gap-y-[62px] md:w-[53.6%]">
+          <AnimatedWrapper
+            animation={fadeInAnimation({ y: 30 })}
+            className="flex flex-col gap-y-[18px] md:flex-col-reverse md:justify-end md:gap-y-[62px] md:w-[53.6%]"
+          >
             <SectionTitle className="md:max-w-[201px] xl:max-w-[321px] md:ml-auto md:mb-[62px]">
               Насолода кожного моменту
             </SectionTitle>
@@ -56,8 +84,11 @@ export default function Mission() {
               вашого тіла та душі. Ми не просто готуємо їжу, а дбаємо
               про здоров’я, енергію та гарний настрій кожного нашого клієнта.
             </p>
-          </div>
-          <div className="w-full md:w-[44.6%] aspect-[300/200] md:aspect-[509/458] rounded-[12px] md:rounded-[18px] overflow-hidden">
+          </AnimatedWrapper>
+          <AnimatedWrapper
+            animation={fadeInAnimation({ x: 30, delay: 0.8 })}
+            className="w-full md:w-[44.6%] aspect-[300/200] md:aspect-[509/458] rounded-[12px] md:rounded-[18px] overflow-hidden"
+          >
             <Image
               src="/images/about/mission/imageThird.webp"
               alt="shawerma"
@@ -65,14 +96,19 @@ export default function Mission() {
               height={1365}
               className="w-full h-full object-cover"
             />
-          </div>
-          <Image
-            src="/images/about/mission/tomatoes.webp"
-            alt="tomatoe"
-            width={696}
-            height={614}
-            className="hidden md:block absolute -z-10 bottom-[-72px] left-[-197px] lg:left-[-127px] xl:left-[-197px] md:w-[384px] xl:w-[481px] h-auto"
-          />
+          </AnimatedWrapper>
+          <AnimatedWrapper
+            animation={fadeInAnimation({ scale: 0.8, delay: 1.6 })}
+            className="hidden md:block absolute -z-10 bottom-[-72px] left-[-197px] lg:left-[-127px] xl:left-[-197px]"
+          >
+            <Image
+              src="/images/about/mission/tomatoes.webp"
+              alt="tomatoe"
+              width={696}
+              height={614}
+              className="md:w-[384px] xl:w-[481px] h-auto"
+            />
+          </AnimatedWrapper>
         </div>
       </div>
     </section>
