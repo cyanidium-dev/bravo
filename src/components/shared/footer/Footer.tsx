158b4f11491b1b7d669c077474df48d4d2a9a7f3
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-29 10:14:51 +0200

    updated footer animation

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -4,11 +4,15 @@ import NavMenu from "./NavMenu";
 import FooterInfo from "./FooterInfo";
 import SocialsList from "./SocialsList";
 import AnimatedWrapper from "@/components/shared/animatedWrappers/AnimatedWrapper";
+import { fadeInAnimation } from "@/helpers/animation";
 
 export default function Footer() {
   return (
-    <footer className="w-dvw pt-20 pb-[50px] md:pb-[62px] bg-black">
-      <AnimatedWrapper className="container xl:max-w-[1280px]">
+    <footer className="w-dvw pt-20 pb-[50px] md:pb-[62px] bg-black overflow-hidden">
+      <AnimatedWrapper
+        animation={fadeInAnimation({ y: 30 })}
+        className="container xl:max-w-[1280px]"
+      >
         <Logo className="text-40bold md:text-64bold text-white" />
         <div className="flex md:flex-col xl:flex-row md:gap-y-8 justify-between mt-[60px] md:mt-9">
           <NavMenu />
