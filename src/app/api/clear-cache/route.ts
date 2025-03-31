c15b4fac32e79756c59ecff3f3a02bfc4d094bb1
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-01 00:41:51 +0300

    update route

--- src/app/api/clear-cache/route.ts
+++ src/app/api/clear-cache/route.ts
@@ -6,7 +6,16 @@ export const dynamic = "force-dynamic"; // Гарантує, що роут не 
 export async function GET() {
   try {
     revalidateTag("datocms");
-    return NextResponse.json({ message: "Кеш оновлено!" });
+
+    return new NextResponse(JSON.stringify({ message: "Кеш оновлено!" }), {
+      status: 200,
+      headers: {
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, proxy-revalidate",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
+    });
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return NextResponse.json(
