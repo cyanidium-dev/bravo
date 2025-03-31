ebee4dad78714271d0878f02cc81841cc5bd481a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-01 02:00:29 +0300

    Update route.ts

--- src/app/api/datocms/route.ts
+++ src/app/api/datocms/route.ts
@@ -1,32 +1,29 @@
 import { NextResponse } from "next/server";
+import axios from "axios";
 import { DATOCMS_URL } from "@/constants/constants";
+import { unstable_cache } from "next/cache";
 
 const DATOCMS_API_TOKEN = process.env.DATOCMS_API_TOKEN || "";
 
-// Функція для отримання даних з DatoCMS з підтримкою кешування через fetch
-const fetchDatoCMS = async (
-  query: string,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  variables: any,
-  includeDrafts: boolean
-) => {
-  const response = await fetch(DATOCMS_URL, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${DATOCMS_API_TOKEN}`,
-      "Content-Type": "application/json",
-      ...(includeDrafts ? { "X-Include-Drafts": "true" } : {}),
-    },
-    body: JSON.stringify({ query, variables }),
-    next: { revalidate: 3600, tags: ["datocms"] }, // Вказуємо тег для кешування
-  });
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch data from DatoCMS");
-  }
-
-  return response.json();
-};
+// Функція для кешування запитів
+const fetchDatoCMS = unstable_cache(
+  async (query: string, variables, includeDrafts: boolean) => {
+    const response = await axios.post(
+      DATOCMS_URL,
+      { query, variables },
+      {
+        headers: {
+          Authorization: `Bearer ${DATOCMS_API_TOKEN}`,
+          "Content-Type": "application/json",
+          ...(includeDrafts ? { "X-Include-Drafts": "true" } : {}),
+        },
+      }
+    );
+    return response.data;
+  },
+  ["datocms"], // Унікальний ключ кешу
+  { revalidate: 3600, tags: ["datocms"] } // Оновлення кешу раз на годину
+);
 
 export async function POST(req: Request) {
   try {
@@ -42,9 +39,8 @@ export async function POST(req: Request) {
     return NextResponse.json(data);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to fetch data from DatoCMS" },
-      { status: 500 }
-    );
+    return NextResponse.json({
+      error: "Failed to fetch data from DatoCMS",
+    });
   }
 }
