11a692d36f3314f618394e016e39ae3c3994c8a5
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-31 17:10:35 +0300

    added cms requests caching for 1hr

--- src/app/api/datocms/route.ts
+++ src/app/api/datocms/route.ts
@@ -1,17 +1,13 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 import { DATOCMS_URL } from "@/constants/constants";
+import { unstable_cache } from "next/cache";
 
 const DATOCMS_API_TOKEN = process.env.DATOCMS_API_TOKEN || "";
 
-export async function POST(req: Request) {
-  try {
-    const { query, variables, includeDrafts } = await req.json();
-
-    if (!query) {
-      return NextResponse.json({ error: "Missing query" }, { status: 400 });
-    }
-
+// Функція для кешування запитів
+const fetchDatoCMS = unstable_cache(
+  async (query: string, variables, includeDrafts: boolean) => {
     const response = await axios.post(
       DATOCMS_URL,
       { query, variables },
@@ -23,8 +19,24 @@ export async function POST(req: Request) {
         },
       }
     );
+    return response.data;
+  },
+  ["datocms_query"], // Унікальний ключ кешу
+  { revalidate: 3600, tags: ["datocms"] } // Оновлення кешу раз на годину
+);
 
-    return NextResponse.json(response.data);
+export async function POST(req: Request) {
+  try {
+    const { query, variables, includeDrafts } = await req.json();
+
+    if (!query) {
+      return NextResponse.json({ error: "Missing query" }, { status: 400 });
+    }
+
+    // Використання кешованої функції
+    const data = await fetchDatoCMS(query, variables, includeDrafts);
+
+    return NextResponse.json(data);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return NextResponse.json({
