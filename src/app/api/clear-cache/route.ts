76aee191afa27ab751c0dc0dc6c454a5abf7b815
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-01 01:23:11 +0300

    Update route.ts

--- src/app/api/clear-cache/route.ts
+++ src/app/api/clear-cache/route.ts
@@ -1,23 +1,10 @@
 import { NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
-export const revalidate = 0;
-
-export const dynamic = "force-dynamic"; // Гарантує, що роут не кешується на Vercel
-
 export async function GET() {
   try {
-    revalidateTag("datocms");
-
-    return new NextResponse(JSON.stringify({ message: "Кеш оновлено!" }), {
-      status: 200,
-      headers: {
-        "Cache-Control":
-          "no-store, no-cache, must-revalidate, proxy-revalidate",
-        Pragma: "no-cache",
-        Expires: "0",
-      },
-    });
+    await revalidateTag("datocms");
+    return NextResponse.json({ message: "Кеш оновлено!" });
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return NextResponse.json(
