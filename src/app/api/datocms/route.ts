2bfa520224694b5a834385c10f8ad22bccd85734
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-01 12:29:32 +0300

    updated datocms route

--- src/app/api/datocms/route.ts
+++ src/app/api/datocms/route.ts
@@ -1,31 +1,8 @@
 import { NextResponse } from "next/server";
+import axios from "axios";
 import { DATOCMS_URL } from "@/constants/constants";
-import { unstable_cache } from "next/cache";
 
 const DATOCMS_API_TOKEN = process.env.DATOCMS_API_TOKEN || "";
-
-// Функція для кешування запитів
-const fetchDatoCMS = unstable_cache(
-  async (query: string, variables, includeDrafts: boolean) => {
-    const response = await fetch(DATOCMS_URL, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${DATOCMS_API_TOKEN}`,
-        "Content-Type": "application/json",
-        ...(includeDrafts ? { "X-Include-Drafts": "true" } : {}),
-      },
-      body: JSON.stringify({ query, variables }),
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch data from DatoCMS");
-    }
-
-    return response.json();
-  },
-  ["datocms"],
-  { tags: ["datocms"] }
-);
 
 export async function POST(req: Request) {
   try {
@@ -35,10 +12,19 @@ export async function POST(req: Request) {
       return NextResponse.json({ error: "Missing query" }, { status: 400 });
     }
 
-    // Використання кешованої функції
-    const data = await fetchDatoCMS(query, variables, includeDrafts);
+    const response = await axios.post(
+      DATOCMS_URL,
+      { query, variables },
+      {
+        headers: {
+          Authorization: `Bearer ${DATOCMS_API_TOKEN}`,
+          "Content-Type": "application/json",
+          ...(includeDrafts ? { "X-Include-Drafts": "true" } : {}),
+        },
+      }
+    );
 
-    return NextResponse.json(data);
+    return NextResponse.json(response.data);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return NextResponse.json({
